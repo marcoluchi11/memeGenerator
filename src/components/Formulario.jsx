1b4c62c0a31aa5cedde88b92742308de0cd43e64
Marco Luchi <marcoaluchi@hotmail.com>
2020-07-01 20:18:32 -0300

    se crea componente form para armar los inputs

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Formulario = () => {
+  return (
+    <Fragment>
+      <input type="text" placeholder="Coloca el texto aqui" />
+    </Fragment>
+  );
+};
+
+export default Formulario;
