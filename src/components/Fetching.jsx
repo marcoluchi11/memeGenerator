2472b2727c9863196182cfa20820d6f139371cb9
Marco Luchi <marcoaluchi@hotmail.com>
2020-07-04 11:22:47 -0300

    se muestra la imagen con el meme creado y sustituye a la plantilla

--- src/components/Fetching.jsx
+++ src/components/Fetching.jsx
@@ -1,12 +1,12 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Formulario from "./Formulario";
 import "./fetching.css";
-import shortid from "shortid";
+import Memecreado from "./Memecreado";
 const Fetching = ({ linkImagen, setClickeado, setid, clickeado, setUrl }) => {
   let { id, url } = linkImagen;
-
+  const [clickSubmit, setClickSubmit] = useState(true);
+  const [memeCreado, setMemeCreado] = useState("");
   const clickMeme = (identifier) => {
-    console.log(identifier);
     setClickeado(true);
     setid(identifier);
     setUrl(url);
@@ -17,14 +17,23 @@ const Fetching = ({ linkImagen, setClickeado, setid, clickeado, setUrl }) => {
         <Fragment>
           <div className="row">
             <div className="col-12 mb-5">
-              <img
-                className="seleccionada"
-                src={linkImagen[0].url}
-                alt={linkImagen[0].id}
-              />
+              {clickSubmit ? (
+                <img
+                  className="seleccionada"
+                  src={linkImagen[0].url}
+                  alt={linkImagen[0].id}
+                />
+              ) : (
+                <Memecreado meme={memeCreado} />
+              )}
             </div>
             <div className="col-12 mb-5">
-              <Formulario id={linkImagen[0].id} setClickeado={setClickeado} />
+              <Formulario
+                setMemeCreado={setMemeCreado}
+                setClickSubmit={setClickSubmit}
+                id={linkImagen[0].id}
+                setClickeado={setClickeado}
+              />
             </div>
           </div>
         </Fragment>
