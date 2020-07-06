957e4e865c9b01d346976041589364f4262c95df
Marco Luchi <marcoaluchi@hotmail.com>
2020-07-06 12:46:22 -0300

    se cambio el nombre por uno con M mayuscula

--- src/components/Memecreado.jsx
+++ src/components/Memecreado.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "./Memecreado.css";
 const Memecreado = ({ meme }) => {
-  return <img src={meme} alt="" />;
+  return <img className="memardo" src={meme} alt="" />;
 };
 
 export default Memecreado;
