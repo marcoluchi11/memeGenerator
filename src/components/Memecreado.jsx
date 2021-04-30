2b1c0c311561d8010221b1f8c8615265ef5b3528
marcoluchi11 <marcoaluchi@hotmail.com>
2021-04-30 18:49:40 -0300

    Se muestra texto y boton en el mismo componente

--- src/components/Memecreado.jsx
+++ src/components/Memecreado.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Fragment } from "react";
 import "./Memecreado.css";
-const Memecreado = ({ meme }) => {
-  return <img className="memardo" src={meme} alt="" />;
+const Memecreado = ({ meme, setClickeado }) => {
+  return (
+    <Fragment>
+      <div className="meme-creado">
+        <img className="memardo" src={meme} alt="" />
+        <h4>Listo, acá tenés tu meme!</h4>
+      </div>
+      <button onClick={() => setClickeado(false)}>Atrás</button>
+    </Fragment>
+  );
 };
 
 export default Memecreado;
